a16616d3c05d7a013a4920b00352b75b7dea201f
Dalton Klein <deklein@live.com>
2021-03-09 15:52:19 -0600

    Fe-Feat/FirstCommit

---
+++ angular-playground/src/app/shop/shop.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+
+  items = [
+    'milk',
+    'eggs',
+    'bread', 
+    'cheese'
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
